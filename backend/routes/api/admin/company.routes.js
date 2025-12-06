5be23bc7f4119a4986ec889eedc8c0c32fa02931
hiralkotwani01 <hiralkotwani01@gmail.com>
2025-12-06 14:18:33 +0530

    added routes

--- backend/routes/api/admin/company.routes.js
+++ backend/routes/api/admin/company.routes.js
@@ -22,5 +22,7 @@ router.get('/:companyId/getcompanybyid', companyController.getCompanyById);
 router.delete('/:companyId/deletecompany', companyController.deleteCompany);
 
 router.post('/:companyId/addpromocode', companyController.addPromoCode);
+router.put('/:companyId/updatepromocode/:promoId', companyController.updatePromoCode);
+router.delete('/:companyId/deletepromocode/:promoId', companyController.deletePromoCode);
 
 module.exports = router;
